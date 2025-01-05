bb8bdd96d7286a99d7bb507c6b699e6b83b46ba9
Mohammad Tanzil Idrisi <tanzilidrisi@gmail.com>
2025-01-05 09:11:27 -0600

    new workin

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/polyflow-dev.github.io/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/polyflow-website/',
+  
 });
